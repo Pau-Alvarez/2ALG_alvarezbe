9c26c4ccb16c759489ca4b79a5f04078ed057895
Pau Alvarez <alvarezbe@esat-alumni.com>
2025-12-12 10:37:18 +0100

    Final Final de verdad

--- DS_ALG/include/adt_memory_node.h
+++ DS_ALG/include/adt_memory_node.h
@@ -20,30 +20,142 @@ typedef struct memory_node_s {
 
 // Memory Node's API Declarations
 struct memory_node_ops_s {
-  void*(*data) (MemoryNode *node);
+  /**
+ * @brief
+ * @param 
+ * @return 
+ */
+//Returns a refernce for a given node data
+  void*(*data) (MemoryNode *node); 
+  /**
+ * @brief 
+ * @param 
+ * @param 
+ * @return 
+ */
+ //stores src into a given node taking up the bytes param of space
   s16(*setData) (MemoryNode *node, void *src, u16 bytes);
+  /**
+ * @brief 
+ * @param 
+ * @return 
+ */
+ //return node size
   u16(*size) (MemoryNode *node);
 
-  s16(*reset) (MemoryNode *node); 		// reset content of memory node
-  s16(*softReset) (MemoryNode *node);	// reset content of memory node without freeing the data
-  s16(*free) (MemoryNode *node);		// free memory, both data and node
-  s16(*softFree) (MemoryNode *node);	// free only the node (its mmory, not its data)
+  /**
+ * @brief 
+ * @param
+ * @return 
+ */
+ //resets the content of a given node
+  s16(*reset) (MemoryNode *node);
 
+  /**
+  * @brief  
+  * @param 
+  * @return 
+  */
+  //resets both the container and content of a given node
+  s16(*softReset) (MemoryNode *node);	
+  /**
+ * @brief  
+ * @param 
+ * @return 
+ */
+ //resets both the container of a given node
+  s16(*free) (MemoryNode *node);
+  /**
+ * @brief 
+ * @param 
+ * @return 
+ */
+ /**/
+//resets and frees the whole node and it's content
+  s16(*softFree) (MemoryNode *node);	// free only the node (its memory, not its data)
+
+  /**
+ * @brief 
+ * @param 
+ * @param 
+ * @return 
+ */
+ //stores given value into the given node
   s16(*memSet) (MemoryNode *node, u8 value);
+  /**
+ * @brief 
+ * @param 
+ * @param 
+ * @param 
+ * @return 
+ */
+ //resizes node with bytes and copies src into existing node data
   s16(*memCopy) (MemoryNode *node, void *src, u16 bytes);
+  /**
+ * @brief 
+ * @param 
+ * @param 
+ * @param 
+ * @return 
+ */
+ //reserves new node size with existing data and bytes
+ //and concats existing data with src
   s16(*memConcat) (MemoryNode *node, void *src, u16 bytes);
+  /**
+ * @brief 
+ * @param 
+ * @param 
+ * @return 
+ */
+ //AND masks a node to only keep bits set as 1
   s16(*memMask) (MemoryNode *node, u8 mask);
-
+/**
+ * @brief 
+ * @param 
+ * @return 
+ */
+ //prints node content
   void(*print) (MemoryNode *node);
-
+/**
+ * @brief 
+ * @param 
+ * @return 
+ */
+ //returns node->data next node
   MemoryNode* (*next)(MemoryNode* node);
+  /**
+ * @brief 
+ * @param 
+ * @param 
+ * @return 
+ */
+ //sets node->data next node
   s16 (*setNext)(MemoryNode* node, MemoryNode* next);
+  /**
+ * @brief 
+ * @param 
+ * @return 
+ */
+ //returns node->data prev node
   MemoryNode* (*prev)(MemoryNode* node);
+  /**
+ * @brief 
+ * @param 
+ * @param 
+ * @return 
+ */
+ //sets node->data prev node
   s16 (*setPrev)(MemoryNode* node, MemoryNode* prev);
 };
 
 MemoryNode* MEMNODE_create();
 s16 MEMNODE_createFromRef(MemoryNode **node);
-s16 MEMNODE_createLite(MemoryNode *node); // Creates a memory node without memory allocation
+/**
+ * @brief 
+ * @param 
+ * @return 
+ */
+ //calls memnode_initwithoutcheck and returns an error if node == NULL
+s16 MEMNODE_createLite(MemoryNode *node);
 
 #endif // __ADT_MEMORY_NODE_H__
